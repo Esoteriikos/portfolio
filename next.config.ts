63a9cfc177d39793c8e05e16fadd2782453b15b0
Esoteriikos <s3071999@gmail.com>
2025-06-03 05:30:00 -0700

    Updated

--- next.config.ts
+++ next.config.ts
@@ -10,11 +10,12 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: 'export',
+  
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? '/portfolio' : '',
-  assetPrefix: isProd ? '/portfolio/' : '',
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
 };
 
 export default nextConfig;
